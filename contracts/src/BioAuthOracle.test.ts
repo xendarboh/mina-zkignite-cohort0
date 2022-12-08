444c8382e3f6704a05a4ca5d52c457eeb0615f1b
Xen <1435589+xendarboh@users.noreply.github.com>
2022-12-08 19:01:58 +0000

    feat(contracts): BioAuthOracle verify and verifyAccount

--- contracts/src/BioAuthOracle.test.ts
+++ contracts/src/BioAuthOracle.test.ts
@@ -9,13 +9,17 @@ import {
   PublicKey,
   AccountUpdate,
   Signature,
+  Poseidon,
+  UInt64,
 } from 'snarkyjs';
 
 import { jest } from '@jest/globals';
 
 // The public key of our trusted data provider
 const ORACLE_PUBLIC_KEY =
-  'B62qoAE4rBRuTgC42vqvEyUqCGhaZsW58SKVW4Ht8aYqP9UTvxFWBgy';
+  'B62qpxcmAq7DFDFk1wGrishcNTEVgwZaVbtsTFEc8CiARFt31oauHsu';
+
+const ORACLE_URL = 'http://localhost:3000';
 
 // fix "fail not defined"
 // https://github.com/facebook/jest/issues/11698#issuecomment-1332760625
@@ -32,8 +36,12 @@ function fail(message = '') {
 let proofsEnabled = false;
 function createLocalBlockchain() {
   const Local = Mina.LocalBlockchain({ proofsEnabled });
+  Local.setTimestamp(UInt64.from(Date.now()));
   Mina.setActiveInstance(Local);
-  return Local.testAccounts[0].privateKey;
+
+  const deployerAccount: PrivateKey = Local.testAccounts[0].privateKey;
+  const userAccount: PrivateKey = Local.testAccounts[1].privateKey;
+  return [deployerAccount, userAccount];
 }
 
 async function localDeploy(
@@ -57,6 +65,7 @@ describe('BioAuthOracle', () => {
   let deployerAccount: PrivateKey,
     zkAppAddress: PublicKey,
     zkAppPrivateKey: PrivateKey;
+  let userAccount: PrivateKey;
 
   beforeAll(async () => {
     await isReady;
@@ -64,7 +73,7 @@ describe('BioAuthOracle', () => {
   });
 
   beforeEach(async () => {
-    deployerAccount = createLocalBlockchain();
+    [deployerAccount, userAccount] = createLocalBlockchain();
     zkAppPrivateKey = PrivateKey.random();
     zkAppAddress = zkAppPrivateKey.toPublicKey();
   });
@@ -84,23 +93,30 @@ describe('BioAuthOracle', () => {
   });
 
   describe('actual API requests', () => {
-    it('emits an `id` event containing the users id if their credit score is above 700 and the provided signature is valid', async () => {
+    it('emits an `verified` event containing the payload if the bioauth, timestamp, and signature are valid', async () => {
       const zkAppInstance = new BioAuthOracle(zkAppAddress);
       await localDeploy(zkAppInstance, zkAppPrivateKey, deployerAccount);
 
-      const response = await fetch(
-        'https://mina-credit-score-signer-pe3eh.ondigitalocean.app/user/1'
-      );
+      // sign the public key to create the payload to bioauthenticate
+      const userPublicKey = userAccount.toPublicKey();
+      const sig = Signature.create(userAccount, userPublicKey.toFields());
+      const sigHash = Poseidon.hash(sig.toFields()).toString();
+
+      const response = await fetch(`${ORACLE_URL}/${sigHash}`);
       const data = await response.json();
 
-      const id = Field(data.data.id);
-      const creditScore = Field(data.data.creditScore);
+      const payload = Field(data.data.payload);
+      const timestamp = Field(data.data.timestamp);
+      const bioAuthId = Field(data.data.bioAuthId);
       const signature = Signature.fromJSON(data.signature);
 
       const txn = await Mina.transaction(deployerAccount, () => {
-        zkAppInstance.verify(
-          id,
-          creditScore,
+        zkAppInstance.verifyAccount(
+          userPublicKey,
+          sig,
+          payload,
+          timestamp,
+          bioAuthId,
           signature ?? fail('something is wrong with the signature')
         );
       });
@@ -109,50 +125,65 @@ describe('BioAuthOracle', () => {
 
       const events = await zkAppInstance.fetchEvents();
       const verifiedEventValue = events[0].event.toFields(null)[0];
-      expect(verifiedEventValue).toEqual(id);
+      expect(verifiedEventValue).toEqual(payload);
     });
 
+    /*
     it('throws an error if the credit score is below 700 even if the provided signature is valid', async () => {
       const zkAppInstance = new BioAuthOracle(zkAppAddress);
       await localDeploy(zkAppInstance, zkAppPrivateKey, deployerAccount);
 
-      const response = await fetch(
-        'https://mina-credit-score-signer-pe3eh.ondigitalocean.app/user/2'
-      );
+      const response = await fetch(`${ORACLE_URL}/2`);
       const data = await response.json();
 
-      const id = Field(data.data.id);
-      const creditScore = Field(data.data.creditScore);
+      const payload = Field(data.data.payload);
+      const timestamp = Field(data.data.timestamp);
+      const bioAuthId = Field(data.data.bioAuthId);
       const signature = Signature.fromJSON(data.signature);
 
       expect(async () => {
         await Mina.transaction(deployerAccount, () => {
           zkAppInstance.verify(
-            id,
-            creditScore,
+            payload,
+            timestamp,
+            bioAuthId,
             signature ?? fail('something is wrong with the signature')
           );
         });
       }).rejects;
     });
+    */
   });
 
   describe('hardcoded values', () => {
-    it('emits an `id` event containing the users id if their credit score is above 700 and the provided signature is valid', async () => {
+    it('emits an `verified` event containing the payload if the bioauth, timestamp, and signature are valid', async () => {
       const zkAppInstance = new BioAuthOracle(zkAppAddress);
       await localDeploy(zkAppInstance, zkAppPrivateKey, deployerAccount);
 
-      const id = Field(1);
-      const creditScore = Field(787);
-      const signature = Signature.fromJSON({
-        r: '13209474117923890467777795933147746532722569254037337512677934549675287266861',
-        s: '12079365427851031707052269572324263778234360478121821973603368912000793139475',
-      });
+      const data = {
+        data: {
+          payload: '1',
+          timestamp: '1670467803093',
+          bioAuthId:
+            '1391777418574392779706621352966998843662998262631589378793598464749407397987',
+        },
+        signature: {
+          r: '3944738628665209662363613258118115619702760042099078922610298516769343672854',
+          s: '21126437327761612007332534776266668885298517943731782631680322180607779695605',
+        },
+        publicKey: 'B62qpxcmAq7DFDFk1wGrishcNTEVgwZaVbtsTFEc8CiARFt31oauHsu',
+      };
+
+      const payload = Field(data.data.payload);
+      const timestamp = Field(data.data.timestamp);
+      const bioAuthId = Field(data.data.bioAuthId);
+      const signature = Signature.fromJSON(data.signature);
 
       const txn = await Mina.transaction(deployerAccount, () => {
         zkAppInstance.verify(
-          id,
-          creditScore,
+          payload,
+          timestamp,
+          bioAuthId,
           signature ?? fail('something is wrong with the signature')
         );
       });
@@ -161,47 +192,62 @@ describe('BioAuthOracle', () => {
 
       const events = await zkAppInstance.fetchEvents();
       const verifiedEventValue = events[0].event.toFields(null)[0];
-      expect(verifiedEventValue).toEqual(id);
+      expect(verifiedEventValue).toEqual(payload);
     });
 
+    /*
     it('throws an error if the credit score is below 700 even if the provided signature is valid', async () => {
       const zkAppInstance = new BioAuthOracle(zkAppAddress);
       await localDeploy(zkAppInstance, zkAppPrivateKey, deployerAccount);
 
-      const id = Field(2);
+      const payload = Field(2);
       const creditScore = Field(536);
       const signature = Signature.fromJSON({
-        r: '25163915754510418213153704426580201164374923273432613331381672085201550827220',
-        s: '20455871399885835832436646442230538178588318835839502912889034210314761124870',
+        r: '2436106470933997614045177223040277450257259428240771442982388663010122787559',
+        s: '758829175171518312245037419834983816096277581883909212570129668321294477673',
       });
 
       expect(async () => {
         await Mina.transaction(deployerAccount, () => {
           zkAppInstance.verify(
-            id,
+            payload,
             creditScore,
             signature ?? fail('something is wrong with the signature')
           );
         });
       }).rejects;
     });
+    */
 
-    it('throws an error if the credit score is above 700 and the provided signature is invalid', async () => {
+    it('throws an error if the provided signature is invalid', async () => {
       const zkAppInstance = new BioAuthOracle(zkAppAddress);
       await localDeploy(zkAppInstance, zkAppPrivateKey, deployerAccount);
 
-      const id = Field(1);
-      const creditScore = Field(787);
-      const signature = Signature.fromJSON({
-        r: '26545513748775911233424851469484096799413741017006352456100547880447752952428',
-        s: '7381406986124079327199694038222605261248869991738054485116460354242251864564',
-      });
+      const data = {
+        data: {
+          payload: '1',
+          timestamp: '1670467803093',
+          bioAuthId:
+            '1391777418574392779706621352966998843662998262631589378793598464749407397987',
+        },
+        signature: {
+          r: '3944738628665209662363613258118115619702760042099078922610298516769343672850', // << wrong
+          s: '21126437327761612007332534776266668885298517943731782631680322180607779695605',
+        },
+        publicKey: 'B62qpxcmAq7DFDFk1wGrishcNTEVgwZaVbtsTFEc8CiARFt31oauHsu',
+      };
+
+      const payload = Field(data.data.payload);
+      const timestamp = Field(data.data.timestamp);
+      const bioAuthId = Field(data.data.bioAuthId);
+      const signature = Signature.fromJSON(data.signature);
 
       expect(async () => {
         await Mina.transaction(deployerAccount, () => {
           zkAppInstance.verify(
-            id,
-            creditScore,
+            payload,
+            timestamp,
+            bioAuthId,
             signature ?? fail('something is wrong with the signature')
           );
         });
