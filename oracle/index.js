49a97ee48a0e0a50f55ce4119685b54132c69aa7
Xen <1435589+xendarboh@users.noreply.github.com>
2022-12-13 02:05:24 +0000

    fix(oracle): install @koa/cors to enable local access

--- oracle/index.js
+++ oracle/index.js
@@ -1,5 +1,6 @@
 const Koa = require("koa");
 const Router = require("@koa/router");
+const cors = require("@koa/cors");
 const {
   Encoding,
   Field,
@@ -13,6 +14,8 @@ const PORT = process.env.PORT || 3000;
 
 const app = new Koa();
 const router = new Router();
+
+app.use(cors());
 
 // simulate authenticated biometric identifiers
 const bioAuthIds = [
